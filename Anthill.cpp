c359e569fb903eb5feda317396fb5c607c7e8da1
Vllad3 <vlad.kiselv.2006@mail.ru>
2025-04-04 19:23:03 +0300

    add func up_lvl and fields

--- Anthill.cpp
+++ Anthill.cpp
@@ -1,1 +1,21 @@
 #include "Anthill.h"
+
+Anthill::Anthill() {
+    this->lvl = 1;
+    this->place_for_ants = 10;
+    this->place_for_food = 50;
+    this->place_for_materials = 50;
+}
+
+Anthill::~Anthill() {
+
+}
+
+void Anthill::up_lvl() {
+    if (this->place_for_materials == 50 + lvl * 20) {
+        this->lvl++;
+        this->place_for_ants += 5;
+        this->place_for_food += 10;
+        this->place_for_materials += 20;
+    }
+}
--- Anthill.h
+++ Anthill.h
@@ -1,12 +1,27 @@
 #ifndef ANTHILL_H
 #define ANTHILL_H
+#include <vector>
 
 
 
 class Anthill {
-
+public:
+    Anthill();
+    ~Anthill();
+    void up_lvl();
+private:
+    int lvl;
+    int place_for_food;
+    int place_for_materials;
+    int place_for_ants;
+    std::vector<> soldiers;
+    std::vector<> builders;
+    std::vector<> cleaners;
+    std::vector<> babysitters;
+    std::vector<> babies;
+    std::vector<> shepherd;
 };
 
 
 
-#endif //ANTHILL_H
+#endif
