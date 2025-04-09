537f7fc4883912cd18157e9973b1cf55bb67605f
SONJIWU28 <kipo30875@gmail.com>
2025-04-09 12:37:07 +0300

    Prototype target with move

--- MAIN (4).cpp
+++ MAIN (4).cpp
@@ -1,17 +1,18 @@
+
 #include <SFML/Audio.hpp>
+#include <sstream>
 #include "Ant.h"
 #include "Enemy.h"
-#include <cmath>
+#include "Anthill.h"
 
 #define Colony
 #ifdef Colony
 int main() {
-    srand(static_cast<unsigned>(time(nullptr)));  
-
-    std::vector<Ant> colony;
+    srand(static_cast<unsigned>(time(nullptr)));
+    Anthill anthill;
     std::vector<Enemy> raid;
     std::vector<Resource> resources;
-    int res_on_map = 0, x = 0, y = 0, ticks = 0;
+    int x = 0, y = 0, ticks = 0;
 
     Clock time;
     float last_time = 0;
@@ -22,21 +23,12 @@ int main() {
         } while ((x > window_weidth / 2 - 3 * start_hill_size) && (x < window_weidth / 2 + 3 * start_hill_size) ||
             (y > window_high / 2 - 3 * start_hill_size) && (y < window_high / 2 - 3 * start_hill_size));
         if (i <= food_cluster_count)
-            create_cluster(res_on_map, resources, x, y, food);
+            create_cluster(resources, x, y, food);
         else
-            create_cluster(res_on_map, resources, x, y, stick);
+            create_cluster(resources, x, y, stick);
     }
 
-    for (int i = 0; i < 20; ++i) {
-        do {
-            x = rand() % window_weidth;
-            y = rand() % window_high;
-        } while ((x < window_weidth / 2 - start_hill_size / 2) ||
-            (x > window_weidth / 2 + start_hill_size / 2) ||
-            (y < window_high / 2 - start_hill_size / 2) ||
-            (y > window_high / 2 + start_hill_size / 2));
-        colony.emplace_back(x, y);
-    }
+    for (int i = 0; i < 20; ++i) anthill.born_baby();
 
     CircleShape circle(start_hill_size);
     circle.setPosition(Vector2f(window_weidth / 2 - start_hill_size, window_high / 2 - start_hill_size));
@@ -44,27 +36,61 @@ int main() {
 
     RenderWindow window(VideoMode(window_weidth, window_high), L"Colony Simulator");
     Image icon;
-    if (!icon.loadFromFile("anthill.png")) {
-        return -1;
-    }
+    if (!icon.loadFromFile("anthill.png")) return -1;
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
     Music backgroundMusic;
-    if (!backgroundMusic.openFromFile("music.ogg")) {
-        return -1;
-    }
+    if (!backgroundMusic.openFromFile("music.ogg")) return -1;
     backgroundMusic.setLoop(true);
     backgroundMusic.setVolume(100);
     backgroundMusic.play();
 
     Event event;
+    Font font;
+    if (!font.loadFromFile("Arial.ttf")) return -1;
+    Text statsText("", font, 20);
+    statsText.setFillColor(Color::White);
+    statsText.setPosition(10, 10);
 
     while (window.isOpen()) {
         if (time.getElapsedTime().asMilliseconds() - last_time >= update_time) {
             ticks++;
             last_time = time.getElapsedTime().asMilliseconds();
 
-            for (auto& ant : colony) {
+            for (auto& ant : anthill.colony) {
+                // Назначаем цель
+                if (!ant.has_valid_target() && ant.get_inventory() == no_res) {
+                    for (auto& res : resources) {
+                        if (res.is_visible()) {
+                            if ((res.get_type() == food && ant.get_role() == 2) ||
+                                (res.get_type() == stick && ant.get_role() == 3)) {
+                                ant.set_target(res.get_posit());
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Подбор ресурса
+                for (auto& res : resources) {
+                    if (res.is_visible() && ant.get_inventory() == no_res) {
+                        if (ant.get_pos().distance(res.get_posit()) < ant_size * 1.5f && ant.pick(res)) {
+                            ant.set_inventory(res.get_type());
+                            res.set_invisible();
+                            ant.set_target(Position(window_weidth / 2, window_high / 2));
+                            break;
+                        }
+                    }
+                }
+
+                // Доставка ресурса
+                if (ant.get_inventory() != no_res && ant.get_pos().in_anthill()) {
+                    ant.drop();
+                    ant.clear_target();
+                }
+
                 ant.move();
+                anthill.feeding();
                 if (ant.get_hp() > 0) {
                     ant.up_time();
                     if (ant.get_age() % stage_time == 0 && ant.get_age())
@@ -72,23 +98,24 @@ int main() {
                 }
             }
 
-            for (size_t i = 0; i < colony.size(); i++) {
-                for (size_t j = i + 1; j < colony.size(); j++) {
-                    Vector2f pos1 = colony[i].get_shape().getPosition();
-                    Vector2f pos2 = colony[j].get_shape().getPosition();
+            // Столкновения муравьёв
+            for (size_t i = 0; i < anthill.colony.size(); i++) {
+                for (size_t j = i + 1; j < anthill.colony.size(); j++) {
+                    Vector2f pos1 = anthill.colony[i].get_shape().getPosition();
+                    Vector2f pos2 = anthill.colony[j].get_shape().getPosition();
 
                     float dx = pos1.x - pos2.x;
                     float dy = pos1.y - pos2.y;
                     float distance = sqrt(dx * dx + dy * dy);
-                    float min_dist = ant_size * 2.0f;  
+                    float min_dist = ant_size * 2.0f;
 
                     if (distance < min_dist && distance > 0.001f) {
                         float overlap = (min_dist - distance) / 2.0f;
                         float offsetX = (dx / distance) * overlap;
                         float offsetY = (dy / distance) * overlap;
 
-                        CircleShape& shape1 = const_cast<CircleShape&>(colony[i].get_shape());
-                        CircleShape& shape2 = const_cast<CircleShape&>(colony[j].get_shape());
+                        CircleShape& shape1 = const_cast<CircleShape&>(anthill.colony[i].get_shape());
+                        CircleShape& shape2 = const_cast<CircleShape&>(anthill.colony[j].get_shape());
 
                         shape1.setPosition(pos1.x + offsetX, pos1.y + offsetY);
                         shape2.setPosition(pos2.x - offsetX, pos2.y - offsetY);
@@ -115,7 +142,7 @@ int main() {
                     float dx = enemy_pos1.x - enemy_pos2.x;
                     float dy = enemy_pos2.y - enemy_pos2.y;
                     float distance = sqrt(dx * dx + dy * dy);
- 
+
                     float radius1 = raid[i].get_shape().getRadius();
                     float radius2 = raid[j].get_shape().getRadius();
                     float min_dist_enemy = radius1 + radius2;
@@ -135,16 +162,31 @@ int main() {
             }
         }
 
+        std::stringstream stats;
+        stats << "Ants: " << anthill.colony.size() << "\n";
+        stats << "Soldiers: " << anthill.get_soldier_count() << "\n";
+        stats << "Builders: " << anthill.get_builder_count() << "\n";
+        stats << "Cleaners: " << anthill.get_cleaner_count() << "\n";
+        stats << "Sitters: " << anthill.get_sitter_count() << "\n";
+        stats << "Babies: " << anthill.get_baby_count() << "\n";
+        stats << "Shepherds: " << anthill.get_shepherd_count() << "\n";
+        stats << "Collectors: " << anthill.get_collector_count() << "\n";
+        stats << "Food: " << anthill.get_food_count() << "\n";
+        stats << "Sticks: " << anthill.get_stick_count() << "\n";
+
+        statsText.setString(stats.str());
+
         while (window.pollEvent(event))
             if (event.type == Event::Closed)
                 window.close();
 
         window.clear(Color(102, 204, 0));
         window.draw(circle);
+        window.draw(statsText);
         for (const auto& res : resources)
             if (res.is_visible())
                 window.draw(res.get_shape());
-        for (auto& ant : colony)
+        for (auto& ant : anthill.colony)
             if (ant.is_visible())
                 window.draw(ant.get_shape());
         for (auto& enemy : raid)
