2f75c1dbf9b800e444ada3c41533744dc931b863
SONJIWU28 <kipo30875@gmail.com>
2025-04-09 12:34:41 +0300

    Prototype move with target

--- Ant.h
+++ Ant.h
@@ -7,7 +7,7 @@ static Role* roles[7] = { new Baby(), new Sitter(), new Collector(), new Builder
 
 class Ant {
 private:
-    int hp, age;
+    int hp, age, max_hp;
     Position pos, target;
     Vector2f velocity;
     Role* role;
@@ -18,6 +18,7 @@ private:
     bool visible;
 public:
     Ant(float x = 0, float y = 0) :pos(x, y), role_id(0), age(0), hp(1), velocity(0, 0), target(0, 0), has_target(false), inventory(no_res) {
+        max_hp = rand() % 201 + 300;
         role = roles[role_id];
         hp = max_hp;
         shape.setRadius(ant_size);
@@ -34,12 +35,20 @@ public:
     void set_velocity(float vx, float vy) { velocity.x = vx; velocity.y = vy; }
     void set_inventory(res_type type) { inventory = type; }
     void up_time() { age++; }
-    void set_target_on_res(vector<Resource>& res);
+    //void set_target_on_res(vector<Resource>& res);
     int get_hp() const { return hp; }
     int get_age() const { return age; }
     int get_role() const { return role_id; }
     bool is_visible() const { return visible; }
     const CircleShape& get_shape() const { return shape; }
+
+    //Target
+    void set_target(const Position& p) { target = p; has_target = true; }
+    void clear_target() { has_target = false; }
+    bool has_valid_target() const { return has_target; }
+    Position get_pos() const { return pos; }
+    res_type get_inventory() const { return inventory; }
+
 };
 
 float randomise_velocity();
