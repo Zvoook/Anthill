dd64fa43077f93922b20b88115fbd1e1ddc08f5a
SONJIWU28 <kipo30875@gmail.com>
2025-04-10 14:12:37 +0300

    new

--- Ant.h
+++ Ant.h
@@ -14,10 +14,12 @@ private:
     int role_id;
     CircleShape shape;
     bool has_target;
+    bool going_home;
     res_type inventory;
     bool visible;
+    float radius_vision;
 public:
-    Ant(float x = 0, float y = 0) :pos(x, y), role_id(0), age(0), hp(1), velocity(0, 0), target(0, 0), has_target(false), inventory(no_res) {
+    Ant(float x = 0, float y = 0) :pos(x, y), role_id(0), age(0), hp(1), velocity(0, 0), target(0, 0), has_target(false), going_home(false), inventory(no_res), radius_vision(80.f) {
         max_hp = rand() % 201 + 300;
         role = roles[role_id];
         hp = max_hp;
@@ -26,29 +28,34 @@ public:
         shape.setPosition(x, y);
         visible = true;
     }
+    bool check_pos() { if (pos.in_anthill()) return 1; return 0; }
+    CircleShape get_vision_circle() const;
     void upd_role();
     void move();
     bool pick(Resource& res);
-    void drop();
+    void set_invisible() { visible = 0; }
     //void work() { role->work(*this); }
     void upd_color();
     void set_velocity(float vx, float vy) { velocity.x = vx; velocity.y = vy; }
     void set_inventory(res_type type) { inventory = type; }
     void up_time() { age++; }
+    void set_hp(int x) { if (hp > 0) hp = x;  else hp = 0; }
     //void set_target_on_res(vector<Resource>& res);
-    int get_hp() const { return hp; }
-    int get_age() const { return age; }
-    int get_role() const { return role_id; }
-    bool is_visible() const { return visible; }
-    const CircleShape& get_shape() const { return shape; }
 
     //Target
     void set_target(const Position& p) { target = p; has_target = true; }
+    void look_around(std::vector<Resource>& resources);
+    // void go_home();
     void clear_target() { has_target = false; }
     bool has_valid_target() const { return has_target; }
     Position get_pos() const { return pos; }
     res_type get_inventory() const { return inventory; }
 
+    int get_hp() const { return hp; }
+    int get_age() const { return age; }
+    int get_role() const { return role_id; }
+    bool is_visible() const { return visible; }
+    const CircleShape& get_shape() const { return shape; }
 };
 
 float randomise_velocity();
