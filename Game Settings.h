9dc6dadfd3164bebf51ee672897f30f83366eacc
SONJIWU28 <kipo30875@gmail.com>
2025-04-10 14:13:02 +0300

    new

--- Game Settings.h
+++ Game Settings.h
@@ -6,45 +6,30 @@
 #include <memory>
 #include <cmath>
 #include <ctime>
-// #include <SFML/Graphics.hpp>
-using namespace std;
-// using namespace sf;
 
+using namespace std;
+using namespace sf;
 
 //Window settings
 const int window_weidth = 1000;
 const int window_high = 1000;
 
 //Timer settings
-const int update_time = 20; //ticks per ms
-const int upd_time_per_sec = (int)1000 / update_time; //ticks per sec
-const int stage_time = 10 * upd_time_per_sec;
+const int scene_update_time = 20; //ticks per ms
+const int ticks_per_second = (int)(1000 / scene_update_time);
+const int stage_time = 7 * ticks_per_second;
 
 //Ant settings
 const int ant_size = 6;
 const int ant_speed = 4;
 const int velocity_changing_period = 10;
 const int life_stages = 5;
-#define max_hp rand() % 201 + 300
-
-//Anthill settings
-#define lvl_1_place_for_ants 30
-#define lvl_1_place_for_food 50000
-#define lvl_1_place_for_materials 1000
-#define lvl_1_count_soldiers 3;
-#define lvl_1_count_builders 3;
-#define lvl_1_count_cleaners 3;
-#define lvl_1_count_sitters 3;
-#define lvl_1_count_babies 3;
-#define lvl_1_count_shepherds 3;
-#define lvl_1_count_collectors 3;
-#define lvl_1_radius 50.f
 
 //Resource settings
 const int count_of_angles_in_form = 30;
-const int small_resource_size = 7;
-const int madium_resource_size = 12;
-const int big_resource_size = 16;
+const int small_resource_size = 1;
+const int madium_resource_size = 5;
+const int big_resource_size = 10;
 const int max_resource_in_claster = 15;
 const int dist_btw_res = 100;
 const int stick_claster_count = 6;
@@ -53,7 +38,12 @@ const int probability_of_small_resources = 70;
 const int probability_of_medium_resources = 20;
 
 //Anthill settings
-const int start_hill_size = 50.f;
+const int start_radius = 50.f;
+const int start_max_ant_count = 20;
+const int start_food_limit = 100;
+const int start_stick_limit = 1000;
+const int start_ant_count = 3;
+const int feeding_period = ticks_per_second;
 
 //Enemy settings
 const int enemy_wave_period = 250;
