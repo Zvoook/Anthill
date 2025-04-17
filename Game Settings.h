8a41be96ed6e37dc05a28b741e69e0f7cd09924f
Dmitry Danilov <rezviipersik@gmail.com>
2025-04-17 22:02:19 +0300

    FINAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Game Settings.h
+++ Game Settings.h
@@ -19,11 +19,11 @@ const int window_height = 1000;
 //Timer settings
 const int fps = 20;
 const int second = 1000 / fps;
-const int feeding_period = 5 * second;           // 5 секунд на кормление
-const int hunger_damage = 2;                     // умеренный голод
+const int feeding_period = 6 * second;           // 5 секунд на кормление
+const int hunger_damage = 1;                     // умеренный голод
 const int stage_time = 6 * second;               // ускоренная стадия взросления
-const int min_born_period = 0.1 * second;          // рождение реже
-const int food_for_born = 20;                    // рождение дороже
+const int min_born_period = 0.2 * second;          // рождение реже
+const int food_for_born = 16;                    // рождение дороже
 
 //Ant settings
 const int max_ants_on_map = 100;
@@ -34,11 +34,11 @@ const bool vision_circle = true;
 const int soldier_radius_vision = 1500.f;
 
 //Resource settings
-const int stick_score = 233;
-const int food_score = 24;
-const int food_cluster_count = 0.006 * window_width;       // немного больше еды
+const int stick_score = 225;
+const int food_score = 30;
+const int food_cluster_count = 0.008 * window_width;       // немного больше еды
 const int stick_claster_count = 0.004 * window_width;      // нормальное число палок
-const int max_resource_in_claster = 30;                    // оставим как есть
+const int max_resource_in_claster = 35;                    // оставим как есть
 const int res_regen_time = 8 * second;
 const int max_res_op_map = 100;
 const int count_of_angles_in_form = 16;
@@ -54,25 +54,25 @@ const int aphid_cluster_count = 0.003 * window_width;
 const int max_aphids_in_cluster = 4;
 const float aphid_speed = 0.2;
 const float aphid_size = ant_size * 0.7f;
-const int aphid_honey_value = 2;
+const int aphid_honey_value = 3;
 const int honey_collect_time = 3 * second;
 
 //Anthill settings
 const int start_radius = 0.07 * window_width;    //начальный радиус муравейника
-const int start_max_ant_count = 35;              // лимит муравьев на старте
-const int start_food_limit = 500;                // лимит еды
-const int start_stick_for_update = 7000;         // палок для первого апгрейда
+const int start_max_ant_count = 45;              // лимит муравьев на старте
+const int start_food_limit = 600;                // лимит еды
+const int start_stick_for_update = 6000;         // палок для первого апгрейда
 const int start_ant_count = 25;                  // 
 const int min_downgrade_period = 7 * second;
 
 //Enemy settings
-const int enemy_damage = 24;
-const int enemy_wave_period = 15 * second;       // рейды каждые 15 секунд
+const int enemy_damage = 22;
+const int enemy_wave_period = 11 * second;       // рейды каждые 15 секунд
 const int min_enemy_in_wave = 2;                 // минимум врагов в рейде
-const int max_diffusion_enemy_in_wave = 10;          // разброс числа врагов в рейде
+const int max_diffusion_enemy_in_wave = 8;          // разброс числа врагов в рейде
 const int enemy_speed = 4;
-const int rob_res_count = 140;
-const int time_without_enemy = (rand()%3 + 2) * enemy_wave_period;       // рейды каждые 15 секунд
+const int rob_res_count = 100;
+const int time_without_enemy = (rand() % 3 + 2) * enemy_wave_period;       // рейды каждые 15 секунд
 
 enum info_type { food_info, stick_info, enemy };
 enum res_type { no_res, food, stick, aphid, body, trash };
